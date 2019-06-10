f6fe239c000f925316e607b3fe672e6b980be380
Dylan <dylanromansky@yahoo.com>
2019-06-09 23:02:47 -0700

    bonus done

---
+++ bonus/run.sh
@@ -0,0 +1,6 @@
+if ($OSTYPE == 'linux-gnu'); then
+	sudo docker run -it --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" doom
+else
+	IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+	docker run -d -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix doom
+fi
