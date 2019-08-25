103bdca30707341b1b2673baac9869fad7ff237d
bigdiggysquiggle <dylanromansky@yahoo.com>
2019-08-25 05:45:38 -0700

    optimized doom container

--- 02_bonus/run.sh
+++ 02_bonus/run.sh
@@ -1,5 +1,6 @@
 if ($OSTYPE == 'linux-gnu'); then
-	sudo docker run --privileged -it --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" doom
+	eval $(docker-machine env default)
+	docker run --privileged -it --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" doom
 else
 	IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 	docker run --privileged -it -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix doom
