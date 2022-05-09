f6ccf8a0b537124452071b674a0fad5141d82b76
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-05-09 11:30:56 -0700

    troubleshooting bug where the container immediately exits

--- school_project/02_bonus/run.sh
+++ school_project/02_bonus/run.sh
@@ -3,7 +3,7 @@
 
 if [ $OSTYPE == 'linux-gnu' ]; then
 #	eval $(docker-machine env default)
-	docker run --privileged -it --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" doom
+	docker run --privileged -it --rm --net=host --env DISPLAY=unix$DISPLAY --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --volume /tmp/.X11-unix:/tmp/.X11-unix doom
 else
 	IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 	docker run --privileged -it -e DISPLAY=$IP:0 -v /tmp/.X11-unix:/tmp/.X11-unix doom
