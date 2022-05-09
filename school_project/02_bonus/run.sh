27f15b9a12f53e5b6835aa7b1a5db5b16191dcbd
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-05-09 11:14:55 -0700

    w

--- school_project/02_bonus/run.sh
+++ school_project/02_bonus/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #currently doesn't properly run the container when ran as a script. commands work when copied out and ran on their own. this is due to the XDG_RUNTIME_DIR not being properly set in the subshell. will play with this later.
 
-if ($OSTYPE == 'linux-gnu'); then
-	eval $(docker-machine env default)
+if [ $OSTYPE == 'linux-gnu' ]; then
+#	eval $(docker-machine env default)
 	docker run --privileged -it --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" doom
 else
 	IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
